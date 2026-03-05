cbc3fac49ecc48cc2631823f168ee80a8f04490e
debashish200 <rdebashish2002@gmail.com>
2026-03-06 00:48:07 +0530

    first commit

---
+++ waste_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Complaint(models.Model):
+    STATUS_CHOICES=[('Pending','Pending'),
+                    ('In Progress','In Progress'),
+                    ('Resolved','Resolved')
+                    ]
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    location=models.CharField(max_length=200)
+    description=models.TextField()
+    status=models.CharField(max_length=20,choices=STATUS_CHOICES,default='Pending')
